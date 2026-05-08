853eca6722ebcde9e80eb89584200039000dee35
manepough <manepough@gmail.com>
2026-05-08 10:53:03 +0800

    renew

--- app/src/main/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-# ManesClient — 32-bit only build
-APP_ABI         := armeabi-v7a
-APP_PLATFORM    := android-19
-APP_STL         := c++_static
-APP_OPTIM       := release
-APP_THIN_ARCHIVE := true
-APP_PIE         := true
