240ab04984bba7250bb6f44b206f46e2b968eb68
manepough <manepough@gmail.com>
2026-05-10 15:18:35 +0800

    Restrict build to 32-bit architecture only

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 # ManesClient — 32-bit only build
-APP_ABI         := armeabi-v7a arm64-v8a
+APP_ABI         := armeabi-v7a
 APP_PLATFORM    := android-19
 APP_STL         := c++_static
 APP_OPTIM       := release
